1bb0d37a4e4c89d091980c2be73ebc47c6aa1282
Anas <anasna6005@gmail.com>
2024-06-05 19:35:05 +0530

    Removed react-quill

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -2,8 +2,8 @@
 import Image from "next/image";
 import styles from "./writePage.module.css";
 import { useState } from "react";
-import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.bubble.css';
+// import ReactQuill from "react-quill";
+// import 'react-quill/dist/quill.bubble.css';
 
 export default function WritePage() {
   const [open, setOpen] = useState(false);
@@ -28,9 +28,9 @@ export default function WritePage() {
             </button>
           </div>
         )}
-        <ReactQuill
+        {/* <ReactQuill
         className={styles.textArea} 
-        theme="bubble" value={value} onChange={setValue}  placeholder="Tell your story..."/>
+        theme="bubble" value={value} onChange={setValue}  placeholder="Tell your story..."/> */}
       </div>
       <button className={styles.publish}>Publish</button>
     </div>
